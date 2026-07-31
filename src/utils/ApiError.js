cf3106224012c7c291295f3e68255276f1564f8a
Harshit Katiyar <harshitknp0@gmail.com>
2026-07-31 12:10:40 +0530

    commented

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -4,6 +4,6 @@ export default class ApiError extends Error {
 
         this.statusCode = statusCode;
 
-        Error.captureStackTrace(this, this.constructor);
+        Error.captureStackTrace(this, this.constructor); // Optional
     }
 }
