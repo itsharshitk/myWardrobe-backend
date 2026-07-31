b55a13a9b4f30b6a4050328cd5a9fdcd05fe595d
Harshit Katiyar <harshitknp0@gmail.com>
2026-07-31 12:02:21 +0530

    error handler

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,9 +1,8 @@
 export default class ApiError extends Error {
     constructor(statusCode, message){
-        super(message);
+        super(message); // Calls the parent(Error)'s constructor to initialize the standard error object
 
         this.statusCode = statusCode;
-        this.message = message;
 
         Error.captureStackTrace(this, this.constructor);
     }
