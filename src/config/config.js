c82d996e6c0111d11d6350ee6ad9893088094382
Harshit Katiyar <harshitknp0@gmail.com>
2026-08-03 23:35:30 +0530

    setup uploading

--- src/config/config.js
+++ src/config/config.js
@@ -3,10 +3,20 @@ dotenv.config();
 
 const config = {
     port: process.env.PORT,
+
     mongoUri: process.env.MONGO_URI,
+    
     nodeEnv: process.env.NODE_ENV,
+    
     jwtRefresh: process.env.JWT_REFRESH,
+    
     jwtAccess: process.env.JWT_ACCESS,
+    
+    cloudinaryName: process.env.CLOUDINARY_NAME,
+    
+    cloudinaryKey: process.env.CLOUDINARY_KEY,
+    
+    cloudinarySecret: process.env.CLOUDINARY_SECRET,
 }
 
 export default config;
